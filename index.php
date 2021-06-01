865ef31be225d71479fd4d0b5721b66fad440901
Ráfaga dos Santos Trentini <rafaga.trentini@gmail.com>
2021-05-31 21:43:29 -0300

    Inicio implementaao upload arquivo

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
     $quantElementos = isset($_POST['quantElementos']) ? $_POST['quantElementos'] : 0;
     $paOuPg = isset($_POST['paOuPg']) ? $_POST['paOuPg'] : "";
     $nomeArquivo = (isset($_POST['nomeArquivo']) ? $_POST['nomeArquivo'] : "") .".json";
-    $resultado = array();
+
+    if($paOuPg == "pa"){
+        $resultado = [];
+        for($k = 1; $k <= $quantElementos; $k++){
+            $resultado[$k] = $a1 + ($k - 1) * $razao;
+        }
+
+    } else if($paOuPg == "pg"){
+        $resultado = [];
+        for($k = 1; $k <= $quantElementos; $k++){
+            $resultado[$k] = $a1 * pow($razao, ($k - 1));
+        }
+    }
 
     $dadosJson = array (
         "a1" => $a1,
@@ -16,33 +28,12 @@
         "resultado" => $resultado
     );
 
-    if($paOuPg == "pa"){
-        calcularPA($a1, $razao, $quantElementos);
-
-    } else if($paOuPg == "pg"){
-        calcularPG($a1, $razao, $quantElementos);
-    }
-
-    function calcularPA($a1, $razao, $quantElementos){
-
-        for($k = 1; $k <= $quantElementos; $k++){
-            $resultado[$k] = $a1 + ($k - 1) * $razao;
-        }
-    }
-
-    function calcularPG($a1, $razao, $quantElementos){
-
-        for($k = 1; $k <= $quantElementos; $k++){
-            $resultado[$k] = $a1 * pow($razao, ($k - 1));
-        }
-    }
-
     $json = json_encode($dadosJson);
     $arquivoJson = fopen($nomeArquivo, 'w');
     fwrite($arquivoJson, $json);
     fclose($arquivoJson);
 ?>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -51,11 +42,22 @@
 </head>
 <style>
 
+    .label {
+        margin-top: 5px;
+    }
+
+    .titulo {
+        font-size: 28px;
+        position: relative;
+        left: 20%;
+    }
+
     .div-criar-json {
         width: 20%;
         position: relative;
         top: 25px;
         left: 25px;
+        align-items: center;
     }
 
     .criar-arquivo-json {
@@ -64,41 +66,50 @@
         align-items: left;
     }
 
-    .label {
-        margin-top: 5px;
+    .div-upload-json {
+        width: 20%;
+        position: relative;
+        top: 25px;
+        left: 25px;;
+        align-items: center;
     }
 
 </style>
 <body>
-    
-    <div class="div-criar-json">
-        <form action="" method="post" class="criar-arquivo-json">
-            <label for="a1" class="label">Informe o a1(Valor inicial): </label>
-            <input type="text" name="a1" id="a1">
+    <div style="width: 100%;">
+        <div class="div-criar-json">
+            <h1 class="titulo">Gerar Json</h1>
+            <form action="" method="post" class="criar-arquivo-json">
+                <label for="a1" class="label">Informe o a1(Valor inicial): </label>
+                <input type="text" name="a1" id="a1">
 
-            <label for="a1" class="label">Informe a razão: </label>
-            <input type="text" name="razao" id="razao">
+                <label for="razao" class="label">Informe a razão: </label>
+                <input type="text" name="razao" id="razao">
 
-            <label for="a1" class="label">Informe a quantidade de elementos: </label>
-            <input type="text" name="quantElementos" id="quantElementos">
+                <label for="quantElementos" class="label">Informe a quantidade de elementos: </label>
+                <input type="text" name="quantElementos" id="quantElementos">
 
-            <label for="a1" class="label">Informe se é pa ou pg: </label>
-            <select name="paOuPg" name="paOuPg" id="paOuPg">
-                <option value=""></option>
-                <option value="pa">PA</option>
-                <option value="pg">PG</option>
-            </select>
+                <label for="paOuPg" class="label">Informe se é pa ou pg: </label>
+                <select name="paOuPg" id="paOuPg">
+                    <option value=""></option>
+                    <option value="pa">PA</option>
+                    <option value="pg">PG</option>
+                </select>
 
-            <label for="nomeArquivo" class="label">Informe o nome do arquivo: </label>
-            <input type="text" id="nomeArquivo">
+                <label for="nomeArquivo" class="label">Informe o nome do arquivo: </label>
+                <input type="text" name="nomeArquivo" id="nomeArquivo">
 
-            <input type="submit" class="label" name="submit1" id="submit1" value="Gerar Json">
-        </form>
+                <input type="submit" class="label" name="submit" id="submit" value="Gerar Json">
+            </form>
+        </div>
 
+        <div class="div-upload-json">
+            <h1>Upload Json</h1>
+            <form action="" method="post">
+                <input type="file" name="uploadJson">
+                <input type="submit" class="label" name="submitUpload" id="submitUpload" value="Upload Arquivo">
+            </form>
+        </div>
     </div>
-
-    <?php
-        
-    ?>
 </body>
 </html>
